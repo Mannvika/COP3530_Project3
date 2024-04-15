e9b597dbc6b40e1625602f382febc337cd18033c
Manav Sanghvi <mannyjainlakes@gmail.com>
2024-04-15 17:15:48 -0400

    Generated graph, and began working on dijkstras

--- project3.py
+++ project3.py
@@ -2,21 +2,23 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import random
 import time
+import sys
 
-# Replace these values with your own
 CLIENT_ID = 'ee38a806ef614c80967d02453cd0c31c'
 CLIENT_SECRET = '1723989c8c6d4f1a8c5b6b54ff40d8b3'
 
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def getGenre(track):
+
+def get_genre(track):
     artist_ids = [artist['id'] for artist in track['artists']]
     artists = sp.artists(artist_ids)['artists']
     genres = []
     for artist in artists:
         genres.extend(artist['genres'])
     return genres
+
 
 def get_random_songs_from_playlist(playlist_id, n):
 
@@ -48,63 +50,52 @@ def get_random_songs_from_playlist(playlist_id, n):
 
     return random_songs
 
+# Dijkstras Algorithm: https://www.geeksforgeeks.org/python-program-for-dijkstras-shortest-path-algorithm-greedy-algo-7/
+def dijkstras_shortest_path(graph, source, num_of_songs):
+
+    vs = {}
+    s = {}
+    dimensions = []
+    predecessors = []
+
+    for i in range(len(graph)):
+        vs.add(i)
+        dimensions[i] = sys.maxsize
+        predecessors[i] = -1
+
+
+
+    return False
+
+
 def main():
-    genres = ['pop', 'rock', 'hip hop', 'country', 'jazz', 'k-pop', 'lo-fi', 'metal']
-
-    # Define query variables
-    minimumYear = ""
-    maximumYear = ""
-    songAmount = ""
-
-    # Get and validate input for minimum year from user
-    valid_input = False
-    while not valid_input:
-        minimumYear = input("Enter a minimum year (Between 1970 and 2024) ")
-        if minimumYear.isnumeric():
-            if 1970 <= int(minimumYear) <= 2024:
-                valid_input = True
-
-    # Save it as an int
-    minimumYear = int(minimumYear)
-
-    # Get and validate input for maximum year from user
-    valid_input = False
-    while not valid_input:
-        maximumYear = input("Enter a maximum year (Between 1970 and 2024) ")
-        if maximumYear.isnumeric():
-            if minimumYear <= int(maximumYear) <= 2024:
-                valid_input = True
-
-    # Save it as an int
-    maximumYear = int(maximumYear)
+    song_amount = ""
 
     # Get and validate input for song number from user
     valid_input = False
     while not valid_input:
-        songAmount = input("Enter number of songs (Between 30 and 50) ")
-        if songAmount.isnumeric():
-            if 30 <= int(songAmount) <= 50:
+        song_amount = input("Enter number of songs (Between 30 and 50) ")
+        if song_amount.isnumeric():
+            if 5 <= int(song_amount) <= 50:
                 valid_input = True
 
     # Save it as an int
-    songAmount = int(songAmount)
+    song_amount = int(song_amount)
 
     print("Generating Playlist...")
 
-    random_tracks = get_random_songs_from_playlist("6yPiKpy7evrwvZodByKvM9", songAmount)
-    print(getGenre(random_tracks[0]))
-
+    random_tracks = get_random_songs_from_playlist("6yPiKpy7evrwvZodByKvM9", 317)
     track_index_map = {}
     for i, track in enumerate(random_tracks):
         track_index_map[i] = track
 
-    #print(track_index_map[0])
-
     graph = [[1000]*len(random_tracks) for i in range(len(random_tracks))]
-    print(graph)
 
-    #for i in range(len(graph)):
-        #for j in range(len(graph[i])):
+    for i in range(len(graph)):
+        for j in range(len(graph[i])):
+            if i != j:
+                graph[i][j] = graph[i][j] - len(set(get_genre(track_index_map[i]))&set(get_genre(track_index_map[j])))
+
 
 if __name__ == "__main__":
     main()
